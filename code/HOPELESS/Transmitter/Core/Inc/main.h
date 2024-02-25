ece1aee6d14f77e8c8cd5860e5f6a1865e8bc197
Aleksandar <ai5517085@gmail.com>
2024-02-25 11:24:52 +0200

    Almost ready transmitter and second version of receiver
    The lord and saviour bestowed upon me the power to complete the transmitter and the second version of the receiver
    NB: the clock of the transmitter must be edited

---
+++ code/HOPELESS/Transmitter/Core/Inc/main.h
@@ -0,0 +1,88 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LEFT_Pin GPIO_PIN_0
+#define LEFT_GPIO_Port GPIOA
+#define RIGHT_Pin GPIO_PIN_1
+#define RIGHT_GPIO_Port GPIOA
+#define HAND_DOWN_Pin GPIO_PIN_2
+#define HAND_DOWN_GPIO_Port GPIOA
+#define HAND_DOWN_EXTI_IRQn EXTI2_IRQn
+#define HAND_UP_Pin GPIO_PIN_3
+#define HAND_UP_GPIO_Port GPIOA
+#define HAND_UP_EXTI_IRQn EXTI3_IRQn
+#define WEAPON_Pin GPIO_PIN_4
+#define WEAPON_GPIO_Port GPIOA
+#define WEAPON_EXTI_IRQn EXTI4_IRQn
+#define NRF24L01_IRQ_Pin GPIO_PIN_9
+#define NRF24L01_IRQ_GPIO_Port GPIOA
+#define NRF24L01_CE_Pin GPIO_PIN_10
+#define NRF24L01_CE_GPIO_Port GPIOA
+#define NRF24L01_CSN_Pin GPIO_PIN_15
+#define NRF24L01_CSN_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
